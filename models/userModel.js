eedb13a2022a51535b5354363b85a270edb0d50e
LuTanDat <ludathoc@gmail.com>
2023-09-20 00:24:37 +0700

    updates

--- models/userModel.js
+++ models/userModel.js
@@ -28,8 +28,18 @@ var userSchema = new mongoose.Schema({
     role: {
         type: String,
         default: "user",
+    },
+    cart: {
+        type: Array,
+        default: [],
+    },
+    address: [{ type: mongoose.Schema.Types.ObjectId, ref: "Address" }],
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+},
+    {
+        timestamps: true,
     }
-});
+);
 
 // ham nay chay truoc khi luu data vao db
 userSchema.pre("save", async function (next) {
