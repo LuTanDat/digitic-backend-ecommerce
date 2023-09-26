16f14f306f2574c5b44b25e2b8d0c65d037e8811
LuTanDat <ludathoc@gmail.com>
2023-09-26 09:31:01 +0700

    updates

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -2,6 +2,8 @@ const User = require("../models/userModel");
 const Product = require("../models/productModel");
 const Cart = require("../models/cartModel");
 const Coupon = require("../models/couponModel");
+const Order = require("../models/orderModel");
+const uniqid = require('uniqid');
 
 const asyncHandler = require("express-async-handler"); // bat loi ma khong can trycatch
 const { generateToken } = require("../config/jwtToken");
@@ -417,6 +419,111 @@ const applyCoupon = asyncHandler(async (req, res) => {
     res.json(totalAfterDiscount);
 });
 
+const createOrder = asyncHandler(async (req, res) => {
+    const { COD, couponApplied } = req.body;
+    const { _id } = req.user;
+    validateMongoDbId(_id);
+    try {
+        if (!COD) throw new Error("Create cash order failed");
+        const user = await User.findById(_id);
+        let userCart = await Cart.findOne({ orderby: user._id });
+        let finalAmout = 0;
+        if (couponApplied && userCart.totalAfterDiscount) {
+            finalAmout = userCart.totalAfterDiscount;
+        } else {
+            finalAmout = userCart.cartTotal;
+        }
+
+        let newOrder = await new Order({
+            products: userCart.products,
+            paymentIntent: {
+                id: uniqid(),
+                method: "COD",
+                amount: finalAmout,
+                status: "Cash on Delivery",
+                created: Date.now(),
+                currency: "usd",
+            },
+            orderby: user._id,
+            orderStatus: "Cash on Delivery",
+        }).save();
+        // update san pham ton kho va da ban
+        let update = userCart.products.map((item) => {
+            return {
+                updateOne: {
+                    filter: { _id: item.product._id },
+                    update: { $inc: { quantity: -item.count, sold: +item.count } },
+                },
+            };
+        });
+        const updated = await Product.bulkWrite(update, {}); // cap nhat hang loat san pham trong DB
+        res.json({ message: "success" });
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const getOrders = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    validateMongoDbId(_id);
+    try {
+        const userorders = await Order.findOne({ orderby: _id })
+            .populate("products.product")
+            .populate("orderby")
+            .exec();
+        res.json(userorders);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const getAllOrders = asyncHandler(async (req, res) => {
+    try {
+        const alluserorders = await Order.find()
+            .populate("products.product")
+            .populate("orderby")
+            .exec();
+        res.json(alluserorders);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const getOrderByUserId = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongoDbId(id);
+    try {
+        const userorders = await Order.findOne({ orderby: id })
+            .populate("products.product")
+            .populate("orderby")
+            .exec();
+        res.json(userorders);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const updateOrderStatus = asyncHandler(async (req, res) => {
+    const { status } = req.body;
+    const { id } = req.params;
+    validateMongoDbId(id);
+    try {
+        const updateOrderStatus = await Order.findByIdAndUpdate(
+            id,
+            {
+                orderStatus: status,
+                paymentIntent: {
+                    status: status,
+                },
+            },
+            { new: true }
+        );
+        res.json(updateOrderStatus);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
 module.exports = {
     createUser,
     loginUserCtrl,
@@ -438,5 +545,6 @@ module.exports = {
     getUserCart,
     emptyCart,
     applyCoupon,
+    createOrder,
 
 }
