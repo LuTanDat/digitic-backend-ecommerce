943341a34affcad7c4163314d5460ba74f1a4877
LuTanDat <ludathoc@gmail.com>
2023-10-11 15:35:53 +0700

    updates_08

--- models/cartModel.js
+++ models/cartModel.js
@@ -3,23 +3,44 @@ const mongoose = require("mongoose"); // Erase if already required
 // Declare the Schema of the Mongo model
 var cartSchema = new mongoose.Schema(
     {
-        products: [
-            {
-                product: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Product",
-                },
-                count: Number,
-                color: String,
-                price: Number,
-            },
-        ],
-        cartTotal: Number, // tong gia goc
-        totalAfterDiscount: Number, // tong gia sau discount
-        orderby: {
+        // products: [
+        //     {
+        //         product: {
+        //             type: mongoose.Schema.Types.ObjectId,
+        //             ref: "Product",
+        //         },
+        //         count: Number,
+        //         color: String,
+        //         price: Number,
+        //     },
+        // ],
+        // cartTotal: Number, // tong gia goc
+        // totalAfterDiscount: Number, // tong gia sau discount
+        // orderby: {
+        //     type: mongoose.Schema.Types.ObjectId,
+        //     ref: "User",
+        // },
+
+        userId: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
         },
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+        },
+        quantity: {
+            type: Number,
+            require: true
+        },
+        price: {
+            type: Number,
+            require: true
+        },
+        color: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Color",
+        }
     },
     {
         timestamps: true,
